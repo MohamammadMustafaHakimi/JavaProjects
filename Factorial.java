5e207664af0a48c029e0cf2c1191b14e3e295553
Mohammad Mustafa Hakimi <mmustafahakimi08@gmail.com>
2024-11-30 01:04:19 +0100

    function has been enhanced

--- Factorial.java
+++ Factorial.java
@@ -1,30 +1,4 @@
-// Student Name: Mohammad Mustafa Hakimi
-// Auaf ID: 48704
-// Factorial
-// For a non-negative integer, the factorial of n, denoted by n!, is the product of all
-// positive integers less than or equal to n
-// n! = n * (n-1) * (n-2) * (n-3 = n-2-1) ... * 1
-// 1! = 1
-// 2! = 2 * 1 = 2
-// 3! = 3 * 2 * 1 = 1 * 2 * 3 = 6
-// 3! = 3
-// 3! = 2
-// 3! = 1
-// 3! = 3 * 2 * 1 = 6
-// 70! = 1.2 * 10^100
-
-
-// 4! = 1
-// fa = factorial*i
-// 4! = 1 * 1 = 1
-// 4! = 1 * 2 = 2
-// 4! = 2 * 3 = 6
-// 4! = 6 * 4 = 24
-
-
-
-
-
+// Program For Calculating Factorial of a given number
 
 public class Factorial {
   public static void main(String[] args) {
